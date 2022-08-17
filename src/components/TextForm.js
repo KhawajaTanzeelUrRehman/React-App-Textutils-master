152aaae6caa1e1d502f812e94b04ca57f17e9c7a
KhawajaTanzeelUrRehman <77752933+KhawajaTanzeelUrRehman@users.noreply.github.com>
2022-08-17 12:26:14 +0500

    final Version

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,10 +19,7 @@ export default function TextForm(props) {
 
     }
     const handleCopy = () => {
-        let newText = document.getElementById("myBox");
-        newText.select();
-        navigator.clipboard.writeText(newText.value);
-        document.getSelection().removeAllRanges();
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied to clipboard", "success");
 
     }
@@ -51,7 +48,7 @@ export default function TextForm(props) {
           </div>
           <div className="container my-3" style={{color: props.mode=== 'dark'?'white': '#042743'}}>
                 <h1>Your text Summary</h1>
-                <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length } characters</p>
+                <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length } characters</p>
                 <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length } Minutes to read</p>
                 <h3>Preview</h3>
                 <p>{text.length>0?text:"Nothing to Preview" }</p>
